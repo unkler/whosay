9095e4bb303c8aadad9c27a022879e8486bf657c
unkler <poru@add-natural.com>
2022-10-24 00:40:29 +0900

    bladeコンポーネント関連追加

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,1 +1,13 @@
-1
+<x-tests.app>
+  <x-slot name="header">
+    ヘッター1です
+  </x-slot>
+  コンポーネント1
+  <x-tests.card title="タイトル1です" content="本文" :message="$message" />
+  <x-tests.card title="タイトル2です" />
+  <x-tests.card title="CSSを変更したい" class="bg-red-300" />
+  <x-slot name="footer">
+    フッター1です
+  </x-slot>
+  
+</x-tests.app>
