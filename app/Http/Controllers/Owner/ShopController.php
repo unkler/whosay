0b8bac4dca8e60a9f52c830eb8203df590ba9f10
unkler <poru@add-natural.com>
2022-10-28 00:15:44 +0900

    shop一覧ベース追加

---
+++ app/Http/Controllers/Owner/ShopController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Owner;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Shop;
+
+class ShopController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+
+        $this->middleware(function ($request, $next) {
+
+            $id = $request->route()->parameter('shop');
+            if (!is_null($id)) {
+                $shopsOwnerId = Shop::findOrFail($id)->owner->id;
+                $ownerId = (int)$shopsOwnerId;
+                $loginId = Auth::id();
+                if ($ownerId !== $loginId) {
+                    abort(404);
+                }
+            }
+            
+
+            return $next($request);
+        });
+    }
+
+    public function index()
+    {
+        $ownerId = Auth::id();
+        $shops = Shop::where('owner_id', $ownerId)->get();
+
+        return view('owner.shops.index', compact('shops'));
+    }
+
+    public function edit($id)
+    {
+        dd(Shop::findOrFail($id));
+    }
+
+    public function update(Request $request, $id)
+    {
+
+    }
+}
