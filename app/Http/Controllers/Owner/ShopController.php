5f74c7f17c07d2a9765c1dab9fcba6fc0e660518
unkler <poru@add-natural.com>
2022-10-28 02:08:55 +0900

    画像アップロード時のバリデーション追加

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use App\Http\Requests\UploadImageRequest;
 use App\Models\Shop;
 use InterventionImage;
 
@@ -46,7 +47,7 @@ class ShopController extends Controller
         return view('owner.shops.edit', compact('shop'));
     }
 
-    public function update(Request $request, $id)
+    public function update(UploadImageRequest $request, $id)
     {
         $imageFile = $request->image;
         if (!is_null($imageFile) && $imageFile->isValid()) {
--- resources/views/owner/shops/edit.blade.php
+++ resources/views/owner/shops/edit.blade.php
@@ -16,7 +16,7 @@
                           <div class="relative">
                             <label for="image" class="leading-7 text-sm text-gray-600">画像</label>
                             <input type="file" id="image" name="image" accept="image/png,image/jpeg,image/jpg" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                            <x-input-error :messages="$errors->get('image')" class="mt-2" />
                           </div>
                         </div>
                         <div class="p-2 w-full flex justify-around mt-4">
