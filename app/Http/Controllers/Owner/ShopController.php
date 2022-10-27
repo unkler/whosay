79b6a2f04119126019608451bc1d1142dc038244
unkler <poru@add-natural.com>
2022-10-28 02:23:12 +0900

    画像アップロード処理をサービスへ切り離し

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use App\Http\Requests\UploadImageRequest;
 use App\Models\Shop;
+use App\Services\ImageService;
 use InterventionImage;
 
 class ShopController extends Controller
@@ -51,17 +52,7 @@ class ShopController extends Controller
     {
         $imageFile = $request->image;
         if (!is_null($imageFile) && $imageFile->isValid()) {
-            // Storage::putFile('public/shops', $imageFile); リサイズ無しの場合
-        
-            $fileName = uniqid(rand() . '_');
-            $extension = $imageFile->extension();
-            $fileNameToStore = $fileName . '.' . $extension;
-
-            $resizedImage = InterventionImage::make($imageFile)
-                ->resize(1920, 1080)
-                ->encode();
-
-            Storage::put('public/shops/'. $fileNameToStore, $resizedImage);
+            $fileNameToStore = ImageService::upload($imageFile, 'shops');
         }
 
         return redirect()->route('owner.shops.index');
