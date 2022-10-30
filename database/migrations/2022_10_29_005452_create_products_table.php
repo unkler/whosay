454b2e1a0f29c9229bf90482e5b54116ef882b67
unkler <poru@add-natural.com>
2022-10-30 20:35:25 +0900

    productのcreate実装

--- database/migrations/2022_10_29_005452_create_products_table.php
+++ database/migrations/2022_10_29_005452_create_products_table.php
@@ -17,7 +17,15 @@ return new class extends Migration
             $table->id();
             $table->foreignId('shop_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
             $table->foreignId('secondary_category_id')->constrained();
+            $table->string('name');
+            $table->text('information');
+            $table->unsignedInteger('price');
+            $table->boolean('is_selling');
+            $table->integer('sort_order')->nullable();
             $table->foreignId('image1')->nullable()->constrained('images');
+            $table->foreignId('image2')->nullable()->constrained('images');
+            $table->foreignId('image3')->nullable()->constrained('images');
+            $table->foreignId('image4')->nullable()->constrained('images');
             $table->timestamps();
         });
     }
