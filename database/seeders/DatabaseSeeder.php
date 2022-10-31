745a65ff4e96d696039bd371a0e4660922df2961
unkler <poru@add-natural.com>
2022-10-31 20:10:12 +0900

    productのfactory追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Product;
+use App\Models\Stock;
 
 class DatabaseSeeder extends Seeder
 {
@@ -27,9 +29,9 @@ class DatabaseSeeder extends Seeder
             ShopSeeder::class,
             ImageSeeder::class,
             CategorySeeder::class,
-            // ProductSeeder::class,
-            // StockSeeder::class,
             UserSeeder::class,
         ]);
+        Product::factory(100)->create();
+        Stock::factory(100)->create();
     }
 }
